4c4da658f28245f8d234745faa47ec1171b081b4
Patiga <dev@patiga.eu>
2025-03-09 11:01:30 +0100

    WGPU: Create Backend Stub

--- src/engine/client/backend_sdl.cpp
+++ src/engine/client/backend_sdl.cpp
@@ -39,6 +39,8 @@
 #include "backend/vulkan/backend_vulkan.h"
 #endif
 
+#include "backend/wgpu/backend_wgpu.h"
+
 #include "graphics_threaded.h"
 
 #include <engine/graphics.h>
@@ -415,6 +417,10 @@ CCommandProcessor_SDL_GL::CCommandProcessor_SDL_GL(EBackendType BackendType, int
 		m_pGLBackend = CreateVulkanCommandProcessorFragment();
 #endif
 	}
+	else if(BackendType == BACKEND_TYPE_WGPU)
+	{
+		m_pGLBackend = CreateWGPUCommandProcessorFragment();
+	}
 #endif
 }
 
@@ -703,6 +709,8 @@ EBackendType CGraphicsBackend_SDL_GL::DetectBackend()
 		RetBackendType = BACKEND_TYPE_VULKAN;
 	else if(pEnvDriver && str_comp_nocase(pEnvDriver, "OpenGL") == 0)
 		RetBackendType = BACKEND_TYPE_OPENGL;
+	else if(pEnvDriver && str_comp_nocase(pEnvDriver, "WGPU") == 0)
+		RetBackendType = BACKEND_TYPE_WGPU;
 	else if(pEnvDriver == nullptr)
 	{
 		// load the config backend
@@ -713,6 +721,8 @@ EBackendType CGraphicsBackend_SDL_GL::DetectBackend()
 			RetBackendType = BACKEND_TYPE_VULKAN;
 		else if(str_comp_nocase(pConfBackend, "OpenGL") == 0)
 			RetBackendType = BACKEND_TYPE_OPENGL;
+		else if(str_comp_nocase(pConfBackend, "WGPU") == 0)
+			RetBackendType = BACKEND_TYPE_WGPU;
 	}
 #else
 	RetBackendType = BACKEND_TYPE_OPENGL;
@@ -779,6 +789,12 @@ void CGraphicsBackend_SDL_GL::ClampDriverVersion(EBackendType BackendType)
 		g_Config.m_GfxGLMinor = gs_BackendVulkanMinor;
 		g_Config.m_GfxGLPatch = 0;
 #endif
+	}
+	else if(BackendType == BACKEND_TYPE_WGPU)
+	{
+		g_Config.m_GfxGLMajor = 24;
+		g_Config.m_GfxGLMinor = 1;
+		g_Config.m_GfxGLPatch = 0;
 	}
 }
 
@@ -886,6 +902,16 @@ bool CGraphicsBackend_SDL_GL::GetDriverVersion(EGraphicsDriverAgeType DriverAgeT
 #else
 		return false;
 #endif
+	}
+	else if(BackendType == BACKEND_TYPE_WGPU)
+	{
+		pName = "WGPU";
+		{
+			Major = 24;
+			Minor = 1;
+			Patch = 0;
+			return true;
+		}
 	}
 	return false;
 }
@@ -1079,7 +1105,13 @@ int CGraphicsBackend_SDL_GL::Init(const char *pName, int *pScreen, int *pWidth, 
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_Config.m_GfxGLMinor);
 	}
 
-	dbg_msg("gfx", "Created %s %d.%d context.", ((m_BackendType == BACKEND_TYPE_VULKAN) ? "Vulkan" : "OpenGL"), g_Config.m_GfxGLMajor, g_Config.m_GfxGLMinor);
+	const char *BackendStr = "OpenGL";
+	if(m_BackendType == BACKEND_TYPE_VULKAN)
+		BackendStr = "Vulkan";
+	else if(m_BackendType == BACKEND_TYPE_WGPU)
+		BackendStr = "WGPU";
+
+	dbg_msg("gfx", "Created %s %d.%d context.", BackendStr, g_Config.m_GfxGLMajor, g_Config.m_GfxGLMinor);
 
 	if(m_BackendType == BACKEND_TYPE_OPENGL)
 	{
